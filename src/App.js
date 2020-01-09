75e9867c213364ee0228126bbc6b1031ed74aaa7
mayashley <54293844+mayashley@users.noreply.github.com>
2020-01-09 11:56:47 -0500

    1-9-2020
    began the tmdb api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.png";
+import Movie from "./Components/Movie";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const movies = [
+  {
+    title: "star wars",
+    id: 0,
+    desc: "all about war in space"
+  },
+  {
+    title: "falling up",
+    id: 1
+  },
+  {
+    title: "the long way home",
+    id: 2
+  },
+  {
+    title: "moby dick",
+    id: 3
+  }
+];
+
+class App extends Component {
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        "https://api.themoviedb.org/3/discover/movie?api_key=625c617a9abf086d7bc551088835e18f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="app-logo" alt="logo" />
+        </header>
+        {movies.map(movie => (
+          <Movie key={movie.id} movie={movie} desc={movie.desc} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
